303af8d91a5e30495548692edcb7e7d286b6823e
Marc Lenselink <marcie_l@hotmail.com>
2017-03-29 10:11:06 +0200

    svm machine learning met rbf 1% dataset + time

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,7 +19,30 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import svm
 
+clf = svm.SVC(kernel='rbf', C=10000., gamma='auto')
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+count = 0
+for i in pred:
+    if i == 1:
+        count += 1
+print "count : ", count
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print accuracy 
 
 
 #########################################################
