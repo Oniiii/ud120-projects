9484d22a8b16c8771e9ccd2f61f0f72ddd738dc8
Marc Lenselink <marcie_l@hotmail.com>
2017-03-31 16:38:14 +0200

    kNearestNeighbors algoritme met n_neighbors = 1

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,8 +30,17 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 
+clf = KNeighborsClassifier(n_neighbors = 1)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test) 
 
+acc = accuracy_score(pred, labels_test)
+print acc
 
 
 
