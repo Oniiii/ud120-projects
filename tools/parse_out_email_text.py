47656cb2f652496de4f6f561720aecf129b775a9
Marc Lenselink <marcie_l@hotmail.com>
2017-04-14 14:23:57 +0200

    used stemming (SnowballStemmer) on string

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,13 +28,19 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
+        text_list = text_string.split()
+        stemmer = SnowballStemmer("english")
+        x=0
+        for word in text_list:
+            words += stemmer.stem(word)
+            x += 1
+            if x < len(text_list): 
+                words += " "
 
 
 
@@ -51,4 +57,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
